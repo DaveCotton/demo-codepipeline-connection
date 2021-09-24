75237381c593884351937bee096614d431fed4f5
DaveCotton <davecotton1@yahoo.com>
2021-09-24 09:38:32 -0700

    corrected target account assumed role

--- iac/components/dev/s3/main.tf
+++ iac/components/dev/s3/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
   region = "us-west-2"
 
   assume_role {
-    role_arn = "arn:aws:iam::${var.target_account_number}:role/cross-account-poc-1"
+    role_arn = "arn:aws:iam::${var.target_account_number}:role/cross-account-poc"
   }
 }
 
